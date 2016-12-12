9eb55310088010fd7c7b95465905f0a7b75c52a9
LoreBerli <mrgorsky91@gmail.com>
2016-12-12 17:26:56 +0100

    Added Hashmap & Consumer

---
+++ src/NgramTask.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by cioni on 11/12/16.
+ */
+public class NgramTask implements Runnable{
+    private List<String> text=new ArrayList<>();
+    private int gram;
+    private RecordHashMap hist;
+
+    public NgramTask(List<String> text, int gram, RecordHashMap map){
+        this.text=text;
+        this.gram=gram;
+        this.hist=map;
+    }
+
+    public void buildHistogram(List<String> words){
+        for(int i=0;i<words.size()-gram+1;i++){
+            List<String> s=words.subList(i,i+gram);
+            hist.containsAndUpdate(s);
+            }
+        }
+
+    @Override
+    public void run(){
+        System.out.println(text.toString());
+        buildHistogram(text);
+        return;
+    }
+
+}
